4ca88e2e1ca65710db53eee6a19331949b364fd1
YarinAshkenazi <yarina@new-image.co.il>
2025-06-16 01:55:32 +0300

    תיקון שירח יתחלף לשמש והפוך בלחיצה

--- services/global.js
+++ services/global.js
@@ -3,10 +3,19 @@ function toggleMenu() {
   navMenu.classList.toggle("open");
 }
 
-  const btn = document.getElementById("toggle-dark");
+
+
+  const btn = document.getElementById("toggle-theme");
+  const icon = document.getElementById("theme-icon");
 
   btn.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+      icon.classList.remove("fa-moon");
+      icon.classList.add("fa-sun");
+    } else {
+      icon.classList.remove("fa-sun");
+      icon.classList.add("fa-moon");
+    }
   });
-
-
